fea62cf0aa09967c2fdfc252f12e2c924af2671c
Sudhir Kumar <sk.sidh@yahoo.com>
2021-04-15 02:44:42 +0000

    Development

---
+++ models/staging/jaffle_Shop/stg_orders.sql
@@ -0,0 +1,14 @@
+
+with orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        _etl_loaded_at
+
+    from {{source('jaffle_shop','orders')}}
+
+)
+select * from orders
